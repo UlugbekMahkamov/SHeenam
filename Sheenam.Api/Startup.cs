5b5df90760d2fae10b6789350a9231f0f0fb14c0
Carter Crons <cartercrons@gmail.com>
2023-02-28 16:50:37 +0500

    CONFIG: Register Broker

--- Sheenam.Api/Startup.cs
+++ Sheenam.Api/Startup.cs
@@ -30,6 +30,7 @@ namespace Sheenam.Api
            
             services.AddControllers();
             services.AddDbContext<StorageBroker>();
+            services.AddTransient<IStorageBroker, StorageBroker>();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Sheenam.Api", Version = "v1" });
