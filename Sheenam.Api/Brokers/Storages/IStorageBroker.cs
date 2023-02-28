7082d6590c767da87593bdbd805e13317ad64998
Carter Crons <cartercrons@gmail.com>
2023-02-28 16:46:46 +0500

    BROKERS: Insert Guest

---
+++ Sheenam.Api/Brokers/Storages/IStorageBroker.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Sheenam.Api.Models.Foundations.Guests;
+
+namespace Sheenam.Api.Brokers.Storages
+{
+    public partial interface IStorageBroker
+    {
+       ValueTask<Guest> InsertGuestAsync(Guest guest);
+    }
+}
